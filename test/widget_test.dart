4e185a3105d793d32269b5533c39dbcd026ef1d0
Susan Kafle <kafle.susan671outlook.com>
2024-01-28 12:32:03 +0545

    asd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testingclass/arithmetic.dart';
 import 'package:testingclass/arithmetic_view.dart';
 
 void main() {
@@ -24,5 +26,33 @@ void main() {
       int actualValue = arithmetic.mult(2, 2);
       expect(actualValue, expectedValue);
     });
+  });
+  testWidgets('add two number', (tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: ArthimeticView(),
+    ));
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextField).first, '5');
+    await tester.enterText(find.byType(TextField).last, '5');
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Add'));
+
+    await tester.pumpAndSettle();
+    expect(find.text('Result is: 10'), findsOneWidget);
+  });
+  testWidgets('sub two number', (tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: ArthimeticView(),
+    ));
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextField).first, '5');
+    await tester.enterText(find.byType(TextField).last, '5');
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Subtract'));
+
+    await tester.pumpAndSettle();
+    expect(find.text('Result is: 0'), findsOneWidget);
   });
 }
